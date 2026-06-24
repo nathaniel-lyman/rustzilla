1bdece400269cc8837ed73dd0536307924dd1451
nathaniel-lyman <nathaniel.lyman@gmail.com>
2026-06-23 21:56:38 -0500

    chore: scaffold rustzilla crate with crossterm

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+pub mod geom;
+pub mod sprite;
+pub mod entity;
+pub mod fish;
+pub mod tank;
+pub mod render;
+pub mod input;
---
+++ src/main.rs
@@ -0,0 +1,3 @@
+fn main() {
+    println!("rustzilla: nothing to see yet");
+}
